124ebe5b6bb57bce97e7afaa8b65de91c8d94703
alexandru-pestritu <147277107+alexandru-pestritu@users.noreply.github.com>
2024-03-12 20:10:24 +0200

    Started layout for word search view

--- Views/WordSearchView.xaml.cs
+++ Views/WordSearchView.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WpfDictionary
+namespace WpfDictionary.Views
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for WordSearchView.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class WordSearchView : Window
     {
-        public MainWindow()
+        public WordSearchView()
         {
             InitializeComponent();
         }
