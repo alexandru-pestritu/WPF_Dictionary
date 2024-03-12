946d0e1af3e0f62a8d2694121056ccec9ec84132
alexandru-pestritu <147277107+alexandru-pestritu@users.noreply.github.com>
2024-03-12 18:37:10 +0200

    Implemented base models

---
+++ Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfDictionary.Models
+{
+    public class Category
+    {
+        public string Name { get; set; }
+
+        public Category() { }
+
+        public Category(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ Models/Word.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfDictionary.Models
+{
+    public class Word
+    {
+        public string Term { get; set; }
+        public string Description { get; set; }
+        public Category Category { get; set; }
+        public string ImagePath { get; set; }    
+
+        public Word() {}
+
+        public Word(string term, string description, Category category, string imagePath = null)
+        {
+            Term = term;
+            Description = description;
+            Category = category;
+            ImagePath = imagePath ?? "/Resources/Images/Words/no-image-available.jpg";
+        }
+    }
+}
